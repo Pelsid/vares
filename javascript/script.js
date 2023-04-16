0cf48aab24c3c305d7a4c39c34b03a28e991fdf7
Maksim <117210740+Pelsid@users.noreply.github.com>
2023-04-17 01:37:20 +0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -85,33 +85,33 @@ const kzt = document.querySelector("#kzt");
 
 // добавляем обработчики кликов на каждый элемент списка
 function selectItem(currency) {
+	let spanText = currency.querySelector('.get__item-span').textContent;
 	course = parseInt(currency.getAttribute("value"));
-	exchangeRate.textContent = `1.00 RUB = ${course} KZT`;
+	exchangeRate.textContent = `1.00 RUB = ${course} ${spanText}`;
 	addImage(currency);
 	getItem();
+	getInput.value = sendInput.value * course;
 }
 
-
-
-  // Функция для добавления картинки и текста в элемент div
-  function addImage(li) {
-    // Удаляем все теги img и span из элемента div
-    getSpan.innerHTML = '';
-    // Создаем новый элемент img
-    const img = document.createElement('img');
-    // Устанавливаем атрибуты для img
-    img.setAttribute('src', li.querySelector('img').getAttribute('src'));
-    img.setAttribute('alt', li.querySelector('img').getAttribute('alt'));
-    img.setAttribute('class', 'get__selected-img');
-    // Создаем новый элемент span
-    const span = document.createElement('span');
-    // Устанавливаем атрибуты для span
-    span.textContent = li.querySelector('.get__item-span').textContent;
-    span.setAttribute('class', 'get__selected-span');
-    // Добавляем img и span в элемент div
-    getSpan.appendChild(img);
-    getSpan.appendChild(span);
-  }
+// Создаем новый элемент img
+const img = document.createElement('img');
+// Создаем новый элемент span
+const span = document.createElement('span');
+// Функция для добавления картинки и текста в элемент div
+function addImage(li) {
+	// Удаляем все теги img и span из элемента div
+	getSpan.innerHTML = '';
+	// Устанавливаем атрибуты для img
+	img.setAttribute('src', li.querySelector('img').getAttribute('src'));
+	img.setAttribute('alt', li.querySelector('img').getAttribute('alt'));
+	img.setAttribute('class', 'get__selected-img');
+	// Устанавливаем атрибуты для span
+	span.textContent = li.querySelector('.get__item-span').textContent;
+	span.setAttribute('class', 'get__selected-span');
+	// Добавляем img и span в элемент div
+	getSpan.appendChild(img);
+	getSpan.appendChild(span);
+}
 
 
 
@@ -122,6 +122,7 @@ const getInput = document.querySelector("#get-input");
 const getLable = document.querySelector("#get-lable");
 const exchangeRate = document.querySelector("#exchange-rate");
 let course = usd.value;
+exchangeRate.textContent = `1.00 RUB = ${course} USD`;
 
 sendInput.addEventListener("input", function () {
 	// устанавливаем значение input2 в значение input1
@@ -135,3 +136,44 @@ sendInput.addEventListener("input", function () {
 		getLable.classList.remove('label--active');
 	}
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// получаем элемент div по id
+const divElement = document.getElementById('qwerty');
+
+// создаем новый объект XMLHttpRequest
+const xhr = new XMLHttpRequest();
+
+// указываем путь к файлу map.svg
+xhr.open('GET', './img/svg/map.svg', true);
+
+// указываем тип ответа как текст
+xhr.responseType = 'text';
+
+// обработчик события, который сработает при успешном завершении запроса
+xhr.onload = function() {
+  // получаем содержимое файла svg
+  const svgContent = xhr.response;
+
+  // создаем новый элемент svg
+  const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+  // устанавливаем содержимое svg в элемент svg
+  svgElement.innerHTML = svgContent;
+
+  // добавляем элемент svg в элемент div
+  divElement.appendChild(svgElement);
+};
+
+// отправляем запрос на сервер
+xhr.send();
